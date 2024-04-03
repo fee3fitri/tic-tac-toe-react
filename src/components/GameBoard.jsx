944454f2ef938abc9c41f46dd0c806e3ce356c31
fee3fitri <110148438+fee3fitri@users.noreply.github.com>
2024-04-02 22:26:34 -0700

    Disable button when it's already clicked

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -31,7 +31,9 @@ export default function GameBoard({ onSelectSquare, turns }) {
       {gameBoard.map((row, rowIndex) => <li key={rowIndex}>
       <ol>
           {row.map((col, colIndex) => <li key={colIndex}>
-            <button onClick={() => onSelectSquare(rowIndex, colIndex)}>{col}</button>
+            <button onClick={() => onSelectSquare(rowIndex, colIndex)} disabled={col !== null}>
+              {col}
+            </button>
           </li>)}
         </ol>
       </li>)}
