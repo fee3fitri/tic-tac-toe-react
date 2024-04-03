13f750fc89a4ca7bbf1be3257f8ae22a86ea865f
fee3fitri <110148438+fee3fitri@users.noreply.github.com>
2024-04-03 11:40:49 -0700

    Checking the draw condition

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 import Player from "./components/Player";
 import GameBoard from "./components/GameBoard";
 import Log from "./components/Log";
+import GameOver from "./components/GameOver";
 
 import { WINNING_COMBINATIONS } from "./winningCombinations";
 
@@ -47,6 +48,8 @@ function App() {
     }
   }
 
+  const isDraw = gameTurns.length === 9 && !winner;
+
   function handleSelectSquare(rowIndex, colIndex) {
     // setActivePlayer(currentPlayer => currentPlayer === "X" ? "O" : "X");
 
@@ -68,7 +71,7 @@ function App() {
           <Player name="Player 1" symbol="X" isActive={activePlayer === "X"} />
           <Player name="Player 2" symbol="O" isActive={activePlayer === "O"} />
         </ol>
-        {winner && <p>You won, {winner}!</p>}
+        {(winner || isDraw) && <GameOver winner={winner} />}
         <GameBoard onSelectSquare={handleSelectSquare} board={gameBoard} />
       </div>
       <Log turns={gameTurns} />
